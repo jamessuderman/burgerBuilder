bde0f8ca85f1b7c391f69565c04d2e83912f044f
James Suderman <jamessuderman@gmail.com>
2018-06-04 10:59:43 -0500

    Added checkout page and routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,31 +76,33 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({loading: true});
+    // this.setState({loading: true});
+    //
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'James Suderman',
+    //     address:  {
+    //       street: '209 Flower Ridge',
+    //       city: 'Fort Worth',
+    //       state: 'TX',
+    //       zipCode: '76108'
+    //     },
+    //     email: 'jamessuderman@gmail.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // };
+    //
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({loading: false, purchasing: false});
+    //     })
+    //   .catch(error => {
+    //     this.setState({loading: false, purchasing: false});
+    //   });
 
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'James Suderman',
-        address:  {
-          street: '209 Flower Ridge',
-          city: 'Fort Worth',
-          state: 'TX',
-          zipCode: '76108'
-        },
-        email: 'jamessuderman@gmail.com'
-      },
-      deliveryMethod: 'fastest'
-    };
-
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({loading: false, purchasing: false});
-        })
-      .catch(error => {
-        this.setState({loading: false, purchasing: false});
-      });
+    this.props.history.push('/checkout');
   };
 
   render() {
